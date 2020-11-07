bb34b51c0af21f0babd1d5b0be8238093a2861b1
Vinícios Oliveira Grama <38024462+vinigr@users.noreply.github.com>
2020-11-06 22:35:12 -0300

    feat(usuario): adicionando validação ao tentar deletar usuário

--- app/Controllers/Http/UsuariosController.ts
+++ app/Controllers/Http/UsuariosController.ts
@@ -7,12 +7,11 @@ import {
   getErroValidacao,
   getMensagemErro,
   capitalize,
-}
-  from 'App/Utils/Utils'
+} from 'App/Utils/Utils'
 import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class UsuarioController {
-  public async cadastro ({ request, response }: HttpContextContract) {
+  public async cadastro({ request, response }: HttpContextContract) {
     try {
       const dadosCadastro = await request.validate({
         schema: schema.create({
@@ -21,9 +20,7 @@ export default class UsuarioController {
             rules.email(),
             rules.unique({ table: 'usuarios', column: 'email' }),
           ]),
-          senha: schema.string({}, [
-            rules.minLength(6),
-          ]),
+          senha: schema.string({}, [rules.minLength(6)]),
         }),
       })
 
@@ -44,13 +41,14 @@ export default class UsuarioController {
     }
   }
 
-  public async login ({ request, response }: HttpContextContract) {
+  public async login({ request, response }: HttpContextContract) {
     try {
       const dadosCadastro = await request.validate({
         schema: schema.create({
           email: schema.string({}, [
             rules.exists({
-              table: 'usuarios', column: 'email',
+              table: 'usuarios',
+              column: 'email',
             }),
           ]),
           senha: schema.string({}, []),
@@ -60,7 +58,7 @@ export default class UsuarioController {
       const usuario = await Usuario.query().where({ email: dadosCadastro.email }).firstOrFail()
       const senhaCorreta = await Hash.verify(usuario.senha, request.input('senha'))
 
-      if(senhaCorreta) {
+      if (senhaCorreta) {
         return response.ok({ token: gerarTokenJWT(usuario) })
       } else {
         return response.status(401).json({ mensagem: 'Credenciais incorretas' })
@@ -74,7 +72,7 @@ export default class UsuarioController {
     }
   }
 
-  public async listagem ({ response }: HttpContextContract) {
+  public async listagem({ response }: HttpContextContract) {
     try {
       return await Usuario.query().select(['id', 'nome', 'email', 'tipo'])
     } catch (error) {
@@ -82,16 +80,16 @@ export default class UsuarioController {
     }
   }
 
-  public async getUsuarioId ({ response, params }: HttpContextContract) {
+  public async getUsuarioId({ response, params }: HttpContextContract) {
     try {
       const usuario = await Usuario.query()
         .where('id', params.id)
         .select(['id', 'nome', 'email', 'tipo'])
         .first()
 
-      if(!usuario) {
+      if (!usuario) {
         return response.status(401).json({
-          'mensagem': 'Usuário não encontrado',
+          mensagem: 'Usuário não encontrado',
         })
       }
 
@@ -101,28 +99,26 @@ export default class UsuarioController {
     }
   }
 
-  public async listagemPorTipo ({ response, params }: HttpContextContract) {
+  public async listagemPorTipo({ response, params }: HttpContextContract) {
     try {
       const tipo = capitalize(params.tipo)
 
       const tipos = ['Cliente', 'Diretor', 'Coordenador', 'Master', 'Professor']
 
-      if(!tipos.includes(tipo)) {
+      if (!tipos.includes(tipo)) {
         return response.status(401).json({
-          'mensagem': 'O tipo de usuário informado não foi reconhecido',
+          mensagem: 'O tipo de usuário informado não foi reconhecido',
         })
       }
 
-      return await Usuario.query()
-        .select(['id', 'nome', 'email', 'tipo'])
-        .where('tipo', tipo)
+      return await Usuario.query().select(['id', 'nome', 'email', 'tipo']).where('tipo', tipo)
     } catch (error) {
       console.log(error)
       return response.badRequest({ error })
     }
   }
 
-  public async cadastroMaster ({ request, response }: HttpContextContract) {
+  public async cadastroMaster({ request, response }: HttpContextContract) {
     try {
       const dadosCadastro = await request.validate({
         schema: schema.create({
@@ -131,14 +127,12 @@ export default class UsuarioController {
             rules.email(),
             rules.unique({ table: 'usuarios', column: 'email' }),
           ]),
-          senha: schema.string({}, [
-            rules.minLength(6),
-          ]),
+          senha: schema.string({}, [rules.minLength(6)]),
           tipo: schema.enum(['Cliente', 'Diretor', 'Coordenador', 'Master', 'Professor']),
         }),
         messages: {
           // eslint-disable-next-line max-len
-          'tipo.enum': 'Tipo de usuário não informado corretamente. Os tipos são Cliente, Diretor, Coordenador, Master e Professor',
+          'tipo.enum': `Tipo de usuário não informado corretamente. Os tipos são Cliente, Diretor, Coordenador, Master e Professor`,
           'email.email': 'Informe um e-mail válido',
         },
       })
@@ -153,7 +147,7 @@ export default class UsuarioController {
         return response.badRequest({ mensagem: 'Email já registrado' })
       }
 
-      if(existeErroValidacao(error)) {
+      if (existeErroValidacao(error)) {
         const erro = getMensagemErro(error)
 
         return response.status(401).json(erro)
@@ -163,20 +157,16 @@ export default class UsuarioController {
     }
   }
 
-  public async atualizarMaster ({ request, response, params }: HttpContextContract) {
+  public async atualizarMaster({ request, response, params }: HttpContextContract) {
     try {
       const data = request.only(['nome', 'senha', 'email', 'tipo'])
 
       await request.validate({
         schema: schema.create({
-          ...(data.nome !== undefined && {nome: schema.string()}),
-          ...(data.email !== undefined && {email: schema.string({}, [
-            rules.email(),
-          ])}),
+          ...(data.nome !== undefined && { nome: schema.string() }),
+          ...(data.email !== undefined && { email: schema.string({}, [rules.email()]) }),
           ...(data.senha !== undefined && {
-            senha: schema.string({}, [
-              rules.minLength(6),
-            ]),
+            senha: schema.string({}, [rules.minLength(6)]),
           }),
           ...(data.tipo !== undefined && {
             tipo: schema.enum(['Cliente', 'Diretor', 'Coordenador', 'Master', 'Professor']),
@@ -184,30 +174,30 @@ export default class UsuarioController {
         }),
         messages: {
           // eslint-disable-next-line max-len
-          'tipo.enum': 'Tipo de usuário não informado corretamente. Os tipos são Cliente, Diretor, Coordenador, Master e Professor',
+          'tipo.enum': `Tipo de usuário não informado corretamente. Os tipos são Cliente, Diretor, Coordenador, Master e Professor`,
           'email.email': 'Informe um e-mail válido',
         },
       })
 
       const usuario = await Usuario.find(params.id)
 
-      if(!usuario) {
+      if (!usuario) {
         return response.status(401).json({
-          'mensagem': 'Usuario não encontrado',
+          mensagem: 'Usuario não encontrado',
         })
       }
 
-      if(data.nome) {
+      if (data.nome) {
         usuario.nome = data.nome
       }
 
-      if(data.email) {
+      if (data.email) {
         const usuarioEmail = await Usuario.findBy('email', data.email)
 
-        if(usuarioEmail) {
-          if(usuarioEmail.id !== usuario.id) {
+        if (usuarioEmail) {
+          if (usuarioEmail.id !== usuario.id) {
             return response.status(401).json({
-              'mensagem': 'E-mail já está vinculado a outro usuário',
+              mensagem: 'E-mail já está vinculado a outro usuário',
             })
           }
         }
@@ -215,11 +205,11 @@ export default class UsuarioController {
         usuario.email = data.email
       }
 
-      if(data.senha) {
+      if (data.senha) {
         usuario.senha = data.senha
       }
 
-      if(data.tipo) {
+      if (data.tipo) {
         usuario.tipo = data.tipo
       }
 
@@ -227,7 +217,7 @@ export default class UsuarioController {
 
       return response.ok({ mensagem: 'Usuário editado com sucesso' })
     } catch (error) {
-      if(existeErroValidacao(error)) {
+      if (existeErroValidacao(error)) {
         const erro = getMensagemErro(error)
 
         return response.status(401).json(erro)
@@ -237,13 +227,21 @@ export default class UsuarioController {
     }
   }
 
-  public async deletarMaster ({ response, params }: HttpContextContract) {
+  public async deletarMaster({ request, response, params }: HttpContextContract) {
     try {
+      const usuarioLogado: Usuario = request.input('usuario')
+
       const usuario = await Usuario.find(params.id)
 
-      if(!usuario) {
+      if (!usuario) {
         return response.status(401).json({
-          'mensagem': 'Usuário não encontrado',
+          mensagem: 'Usuário não encontrado',
+        })
+      }
+
+      if (usuarioLogado.id === usuario.id) {
+        return response.status(401).json({
+          mensagem: 'Não é possível apagar sua própria conta',
         })
       }
 
